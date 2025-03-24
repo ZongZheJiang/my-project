5ba7513d0a19d18dbfa726af3a91ef4335efb486
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-03-25 02:17:17 +0800

    added GitHub and LinkedIn links in homepage, added support for Footer component

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import NavBar from "./navbar";
 import Card from "./card";
 import IntroBanner from "./intro-banner";
 import Hero from "./hero";
+import Footer from "./footer";
 
 export {
-    NavBar, Card, IntroBanner, Hero
+    NavBar, Card, IntroBanner, Hero, Footer
 }
--- src/components/intro-banner.jsx
+++ src/components/intro-banner.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import selfPortrait from '../assets/self-portrait.jpeg';
+import {Link} from 'react-router-dom';
+
 
 const IntroBanner = () => {    
     return (
@@ -13,6 +15,8 @@ const IntroBanner = () => {
             <div className="card-body">
                 <h2 className="card-title">Jiang Zong Zhe</h2>
                 <p>Hello! I am a Year 2 Computer Science student from Nanyang Technological University. I enjoy creating applications.</p>
+                <button class="btn btn-primary"><Link to="https://github.com/ZongZheJiang">My GitHub Page</Link></button>
+                <button class="btn btn-primary"><Link to="https://www.linkedin.com/in/jiang-zong-zhe-047587240/">My LinkedIn Page</Link></button>
                 <div className="card-actions justify-end">
                 </div>
             </div>
