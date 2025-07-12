f6e221fdb2c05cbd418ee32d07470375f63a980d
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-07-13 00:09:44 +0800

    reinstalled dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,9 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    require("daisyui")
   ],
   daisyui: {
     themes: ["light", "dark"], // or specify your themes
   }, 
-
 }
