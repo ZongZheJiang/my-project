9cea627b4e968696e3562facaf0bc844e8c598da
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-07-13 01:30:05 +0800

    trying to fix deps 6.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,11 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("daisyui")
+    require('daisyui'),
   ],
-  daisyui: {
-    themes: ["light", "dark"], // or specify your themes
-  }, 
 }
